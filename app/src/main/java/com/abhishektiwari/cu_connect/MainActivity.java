8b350ca8b996240cc3cca3a97adf58796af5a344
abhis <abhishektiwari2479@gmail.com>
2022-08-18 20:20:58 +0530

    abhishek2

--- app/src/main/java/com/abhishektiwari/cu_connect/MainActivity.java
+++ app/src/main/java/com/abhishektiwari/cu_connect/MainActivity.java
@@ -3,6 +3,8 @@ package com.abhishektiwari.cu_connect;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hello world");
         String a="kb";
+    }
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.profile:
+                Toast.makeText(this, "Profile", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.companion:
+                Toast.makeText(this, "Companion", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.home:
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search:
+                Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.setting:
+                Toast.makeText(this, "Setting", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
