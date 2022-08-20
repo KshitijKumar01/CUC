d8b6046993394aceb2d0ddad0149d435bf64b2cb
abhis <abhishektiwari2479@gmail.com>
2022-08-20 18:32:59 +0530

    abhishek6

--- app/src/main/java/com/abhishektiwari/cu_connect/MainActivity.java
+++ app/src/main/java/com/abhishektiwari/cu_connect/MainActivity.java
@@ -1,22 +1,36 @@
 package com.abhishektiwari.cu_connect;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
 import androidx.core.widget.ImageViewCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    //bottom nevigation
     CardView profilebg,companionbg,homebg,searchbg,settingbg;
+    FloatingActionButton floatingActionButton;
+
+    //drawer layout
+    DrawerLayout drawerLayout;
+    MaterialToolbar toolbar;
+    NavigationView navigationView;
 
 
     @Override
@@ -25,51 +39,151 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.println("Hello world");
 
+        //Bottomnevigation
         profilebg=findViewById(R.id.profileitem);
         searchbg=findViewById(R.id.searchitem);
         settingbg=findViewById(R.id.settingitem);
         homebg=findViewById(R.id.homeitem);
         companionbg=findViewById(R.id.companionitem);
-
-        profilebg.setBackgroundColor(ContextCompat.getColor(this, R.color.black));
-
-
-
-
-        FloatingActionButton floatingActionButton=(FloatingActionButton)findViewById(R.id.home);
-        ImageViewCompat.setImageTintList(
-                floatingActionButton,
-                ColorStateList.valueOf(Color.WHITE)
-        );
-
-
-
+        floatingActionButton=(FloatingActionButton)findViewById(R.id.home);
+        HomeClicked();
         getSupportFragmentManager().beginTransaction().add(R.id.maincontainer,new Home()).commit();
 
+        //drawerlayout
+        toolbar=findViewById(R.id.drawer_toolbar);
+        drawerLayout=findViewById(R.id.drawer);
+        navigationView=findViewById(R.id.navigation_view);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id=item.getItemId();
+                drawerLayout.closeDrawer(GravityCompat.START);
+                switch (id)
+                {
+                    case R.id.about_app:
+
+                        break;
+                    case R.id.connect_to_us:
+
+                        break;
+
+                    case R.id.our_more_apps:
+
+                        break;
+                    case R.id.privacy_policy:
+
+                        break;
+
+                    default:
+                        return true;
+
+
+                }
+                return true;
+            }
+        });
+
     }
+
+
+
+    private void ChangenavbarBackground(int a) {
+        if(a==1)
+        {
+            HomenotClicked();
+            profilebg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.silver)));
+            settingbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            searchbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            companionbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+
+        }
+        else if(a==2)
+        {
+            HomenotClicked();
+            profilebg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            settingbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            searchbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            companionbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.silver)));
+        }
+        else if(a==3)
+        {
+            profilebg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            settingbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            searchbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            companionbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+
+            HomeClicked();
+        }
+        else if(a==4)
+        {
+            HomenotClicked();
+            profilebg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            settingbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            searchbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.silver)));
+            companionbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+
+        }
+        else if(a==5)
+        {
+            HomenotClicked();
+            profilebg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            settingbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.silver)));
+            searchbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+            companionbg.setBackgroundTintList(  ColorStateList.valueOf(getResources().getColor(R.color.transparent)));
+
+        }
+
+
+    }
+
+    private void HomenotClicked() {
+        ImageViewCompat.setImageTintList(
+                floatingActionButton,
+                ColorStateList.valueOf(getResources().getColor(R.color.silver))
+        );
+    }
+
+    private void HomeClicked() {
+        ImageViewCompat.setImageTintList(
+                floatingActionButton,
+                ColorStateList.valueOf(getResources().getColor(R.color.black))
+        );
+    }
+
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.profile:
 
                 getSupportFragmentManager().beginTransaction().replace(R.id.maincontainer,new Profile()).commit();
+                ChangenavbarBackground(1);
                 break;
             case R.id.companion:
 
                 getSupportFragmentManager().beginTransaction().replace(R.id.maincontainer,new Companion()).commit();
+                ChangenavbarBackground(2);
 
                 break;
             case R.id.home:
 
+                ChangenavbarBackground(3);
                 getSupportFragmentManager().beginTransaction().replace(R.id.maincontainer,new Home()).commit();
 
                 break;
             case R.id.search:
 
+                ChangenavbarBackground(4);
                 getSupportFragmentManager().beginTransaction().replace(R.id.maincontainer,new Search()).commit();
 
                 break;
             case R.id.setting:
 
+                ChangenavbarBackground(5);
                 getSupportFragmentManager().beginTransaction().replace(R.id.maincontainer,new Setting()).commit();
 
                 break;
