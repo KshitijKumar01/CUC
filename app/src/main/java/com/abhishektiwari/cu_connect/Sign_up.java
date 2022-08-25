df88ce3015f3979e026c9805dc83534e641789f5
abhis <abhishektiwari2479@gmail.com>
2022-08-26 00:38:46 +0530

    abhishek14

--- app/src/main/java/com/abhishektiwari/cu_connect/Sign_up.java
+++ app/src/main/java/com/abhishektiwari/cu_connect/Sign_up.java
@@ -1,7 +1,9 @@
 package com.abhishektiwari.cu_connect;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,12 +11,21 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Sign_up extends Fragment {
+    CardView next;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+        next=view.findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(),MainActivity.class);
+                getContext().startActivity(intent);
+            }
+        });
         return view;
     }
 }
