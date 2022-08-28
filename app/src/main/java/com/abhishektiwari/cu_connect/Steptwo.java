b90dc086a9a604e32a0a042413029a653c3af2ff
abhis <abhishektiwari2479@gmail.com>
2022-08-29 00:45:23 +0530

    abhishek16

--- app/src/main/java/com/abhishektiwari/cu_connect/Steptwo.java
+++ app/src/main/java/com/abhishektiwari/cu_connect/Steptwo.java
@@ -2,19 +2,23 @@ package com.abhishektiwari.cu_connect;
 
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 
 public class Steptwo extends Fragment {
 
     EditText otp1,otp2,otp3,otp4;
-
+    CardView views,verifyotpbtn;
+    boolean opened;
 
     public Steptwo() {
 
@@ -37,6 +41,15 @@ public class Steptwo extends Fragment {
         otp2=view.findViewById(R.id.otp2);
         otp3=view.findViewById(R.id.otp3);
         otp4=view.findViewById(R.id.otp4);
+        views = view.findViewById(R.id.otpbox);
+        views.setVisibility(View.INVISIBLE);
+        verifyotpbtn=view.findViewById(R.id.verifynumber);
+        verifyotpbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openmenu();
+            }
+        });
 
         EditText[] edit = {(EditText) otp1, (EditText) otp2,(EditText) otp3, (EditText) otp4};
 
@@ -49,4 +62,30 @@ public class Steptwo extends Fragment {
 
         return  view;
     }
+    private void openmenu() {
+        if(!opened){
+            views.setVisibility(View.VISIBLE);
+            TranslateAnimation animate = new TranslateAnimation(
+                    -views.getWidth(),
+                   0,
+                    0,
+                    0);
+            animate.setDuration(1000);
+            animate.setFillAfter(true);
+            views.startAnimation(animate);
+        } else {
+            views.setVisibility(View.INVISIBLE);
+            TranslateAnimation animate = new TranslateAnimation(
+                   0,
+                    -views.getWidth(),
+                    0,
+                   0);
+            animate.setDuration(1000);
+            animate.setFillAfter(true);
+            views.startAnimation(animate);
+        }
+        opened = !opened;
+    }
+
+
 }
